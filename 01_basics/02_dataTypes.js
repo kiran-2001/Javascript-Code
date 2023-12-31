03c5a15c99c71d4def00227b9906eac7f467da4f
Kiran <kirankolte2156@gmail.com>
2023-12-31 12:57:00 +0530

    variables basic

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";  //treat all JS code as newer version
+
+// alert("hellow")  we are using nodejs npr browser
+
+let name = "kiran"     // dataType - string
+let age = 23           // number
+let isLoggendIn = true // boolean
+
+
+// number
+// string
+// boolean
+// null - standalone value
+// undefined
+//symbol -  symbol
+
+// object
+console.log(typeof "kiran")  // string
+console.log(typeof age)  // number
+console.log(typeof null)     // objetct
+console.log(typeof undefined) // undefined
