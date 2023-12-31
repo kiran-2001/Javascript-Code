804db7e836c80dabd61ec4d75ebd8458239de46d
Kiran <kirankolte2156@gmail.com>
2023-12-31 13:35:06 +0530

    conversions of data type

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,43 @@
+
+let score = "33"
+let score1 = "abc77"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+let value = Number(score1)
+console.log(typeof valueInNumber)
+console.log(typeof value)
+
+console.log(value);
+console.log(valueInNumber);
+
+
+// "33" => 33
+//"abc77" => NaN
+// true  => 1; false => 0
+
+
+let isLoggedIn =  "kiran";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+// 1 => true
+// 0 => false
+// "" => false
+// "kiran" => true
+
+
+
+let num = 33
+
+let stringNum = String(num);
+
+console.log(typeof stringNum)
+console.log(stringNum);
+
+
